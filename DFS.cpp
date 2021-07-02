f31c6513b8a845c6b1c58df9f0a26f86b0e6cf52
aditya_jha4 <aditya.jha@rapyuta-robotics.com>
2021-07-02 06:44:29 +0530

    refactored DFS

--- DFS.cpp
+++ DFS.cpp
@@ -1,63 +1,63 @@
-#include <iostream>
-#include <list>
+#include<bits/stdc++.h>
 using namespace std;
 
-list<int>Adj[10];
-int parent[10];
-int N;
-
+const int M = 100;
+vector<int> Adj[M];
+int parent[M];
 
 void init(){
-
-    for(int i=1;i<=N;i++){
-        Adj[i].clear();
+    for(int i=0;i<M;i++){
         parent[i]=-1;
+        Adj[i].clear();
     }
-
     return;
 }
 
 void dfs(int u, int p){
-
     int v;
     for(auto it=Adj[u].begin();it!=Adj[u].end();it++){
-        v=*it;
-        if(v!=p){
-            parent[v]=u;
-            dfs(v,parent[v]);
+        v = *it;
+        if(v != p){
+            parent[v] = u;
+            dfs(v, u);
+        }
+    }    
+    return;
+}
+
+void dfs_util(int V){
+    for(int i=1;i<=V;i++){
+        if(parent[i]==-1){
+            dfs(i, -1);    
         }
     }
     return;
 }
 
-
-void addEdge(int u,int v){
+void add_edge(int u, int v){
     Adj[u].push_back(v);
     Adj[v].push_back(u);
     return;
 }
 
-void display(){
-    for(int i=1;i<=5;i++){
-        cout<<parent[i]<<"-->"<<i<<"\n";
-    }
+void display(int V){
+    for(int i=1;i<=V;i++){cout<<parent[i]<<"--->"<<i<<"\n";}
     return;
 }
 
 int main(){
-
+    int V=5;
+    
     init();
-
-    N=5;
-
-    addEdge(1,2);
-    addEdge(1,3);
-    addEdge(2,4);
-    addEdge(2,5);
-    // addEdge(4,5);                           // this edge creates a cycle, commenting this out will lead to stack overflow 
-
-    dfs(1,-1);
-    display();
-
+    
+    add_edge(1,2);
+    add_edge(1,3);
+    add_edge(2,4);
+    add_edge(2,5);
+    // add_edge(4,5);                       // cyclic graph, if uncommented
+    
+    dfs_util(V);
+    
+    display(V);
     return 0;
 }
