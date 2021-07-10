9684fa027f7af698e6d775654eeab7c9bbf75e68
aditya_jha4 <aditya.jha@rapyuta-robotics.com>
2021-07-10 22:30:50 +0530

    addde method to display primes and a little docs

--- sieve_of_eratosthenes.cpp
+++ sieve_of_eratosthenes.cpp
@@ -5,13 +5,11 @@ using namespace std;
 #define M 1000*1000 + 7
 
 bool prime[M];
-ll spf[M];
+int spf[M];
 
 void sieve_of_eratosthenes(){
-
     for(int i=0;i<M;i++){prime[i]=true;spf[i]=i;}
-
-    for(int i=2;i*i<M;i++){                         //* i*i<M
+    for(int i=2;i*i<M;i++){                         //* i*i<M  cause the smallest prime factor of a composite should always be lesser than sqrt(n)
         if(prime[i]){
             for(int j=2*i;j<M;j+=i){                //* j<M
                 prime[j]=false;
@@ -19,12 +17,25 @@ void sieve_of_eratosthenes(){
             }
         }
     }
+    return;
+}
+
+void show_primes(int cnt){
+    cout<<"showing first "<<cnt<<" prime numbers\n";
+    for(int i=1;i<100;i++){
+        if(prime[i]){cout<<i<<" ";}
+    }
+    cout<<"\n";
+    return;
 }
 
 int main(){
-
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     sieve_of_eratosthenes();
+    int cnt=100;
+    show_primes(cnt);
     return 0;
 }
+// Time complexity: O(n*log(log(n)))
+// (n/2) + (n/3) + (n/5) + (n/7) ... = n(1/2 + 1/3 + 1/5 + 1/7 ...) = n*(log(log(n)))
