6c5ba65061a908fa45e5e62e47e99e7ca9559a55
aditya_jha4 <aditya.jha@rapyuta-robotics.com>
2021-07-13 07:28:01 +0530

    refactored entirely

--- longest_common_substring.cpp
+++ longest_common_substring.cpp
@@ -1,37 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define ll long long int
-
 int get_lcs(string str1, string str2){
 
     int n = str1.length();
     int m = str2.length();
+
+    int res=0;
     
-    int dp[n+1][m+1];
+    int dp[n+1][m+1];                   // dp[i][j] strores the logest common suffix of X[0..i-1] and Y[0..j-1] 
     
     for(int i=0;i<=n;i++){
         for(int j=0;j<=m;j++){
-            if(i==0 || j==0){
-                dp[i][j] = 0;
-            }
+            if(i==0 || j==0){ dp[i][j] = 0;}
             else
-            if(str1[i-1]==str2[j-1]){
-                dp[i][j] = 1+dp[i-1][j-1];
+            if(str1[i-1] == str2[j-1]){
+                dp[i][j] =1+dp[i-1][j-1];
+                res = max(res, dp[i][j]);
             }
             else{
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                dp[i][j] = 0;
             }
-        }   
+            
+        }
     }
-
-    return dp[n][m];
+    return res;
 }
 
 int main(){
 
-    string str1 = "AGGTAB";
-    string str2 = "GXTXAYB";
+    string str1 = "OldSite:GeeksforGeeks.org";
+    string str2 = "NewSite:GeeksQuiz.com";
     cout<<get_lcs(str1, str2)<<"\n";
     return 0;
 }
