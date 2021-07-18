00948b81b0ecd3945afc45737b8500773692804e
aditya_jha4 <aditya.jha@rapyuta-robotics.com>
2021-07-19 03:05:35 +0530

    refactored wildcard_matching

--- wildcard_matching.cpp
+++ wildcard_matching.cpp
@@ -1,52 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long int
-
-bool wildcart_matching(string text, string pattern){
-
+bool wildcard_matching(string text, string pattern){
+    
     int n = text.length();
     int m = pattern.length();
-
-    text = '*' + text;
-    pattern = '*' + pattern;
-
-    bool dp[n+5][m+5];
-
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<=m;j++){
+    
+    bool dp[n+1][m+1];
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
             dp[i][j] = false;
         }
     }
-
-    dp[0][0]=true;
-    int start = 1;
-    while(pattern[start]=='*'){
-        dp[0][start] = true;
-        start++;
+    
+    dp[0][0] = true;
+    
+    for(int j=1;j<=m;j++){
+        if(pattern[j-1]=='*'){                      // empty string can be matched by '*'.
+            dp[0][j] = dp[0][j-1];                 
+        }
     }
-
+    
     for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
-            if(pattern[j] == '*'){
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            if(pattern[j-1]=='*'){
+                dp[i][j] = dp[i][j-1] || dp[i-1][j];
             }
             else
-            if(pattern[j] == '?' || pattern[j] == text[i]){
+            if(pattern[j-1]=='?' || text[i-1]==pattern[j-1]){
                 dp[i][j] = dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = false;
             }
         }
     }
-
     return dp[n][m];
 }
 
-int main(){
-
-    string text = "xyxzzxy";
-    string pattern = "x***x?";
-
-    cout<<wildcart_matching(text, pattern)<<"\n";
-
+int main() {
+    string text = "baaabab";
+    string pattern = "*****ba*****ab";
+    cout<<wildcard_matching(text, pattern);
     return 0;
 }
