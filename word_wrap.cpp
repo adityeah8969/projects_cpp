cef43186f5c3e832466a8f8275b5d22ec4ade0c0
aditya_jha4 <aditya.jha@rapyuta-robotics.com>
2021-07-22 08:13:43 +0530

    refactored word wrap and added comments

--- word_wrap.cpp
+++ word_wrap.cpp
@@ -1,62 +1,68 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define ll long long int
+#define INF INT_MAX
 
-
-int word_wrap(int l[], int n, int M){
-
-    int spaces[n+5][n+5];
-    int dp[n+5][n+5];
-    int c[n+5];
-
-    vector<int> words;
-    words.push_back(-1);    //*
-
-    for(int i=0;i<n;i++){words.push_back(l[i]);}
-
-    for(int i=1;i<=n;i++){
-        spaces[i][i] = M - words[i];
-        for(int j=i+1;j<=n;j++){
-            spaces[i][j] = spaces[i][j-1] - words[j] -1;
-        }
-    }
-
-    for(int i=1;i<=n;i++){
-        for(int j=i;j<=n;j++){
-            if(spaces<0){dp[i][j] = 0;}
-            else{dp[i][j] = spaces[i][j]*spaces[i][j];}
-        }
-    }
-
-    c[0] = 0;
-    for(int j=1;j<=n;j++){
-        c[j] = INT_MAX;
-
-        for(int i=1;i<=j;i++){
-            if(c[i-1]!=INT_MAX && dp[i][j]!=INT_MAX && c[i-1]+dp[i][j]<c[j]){
-                c[j] = c[i-1]+dp[i][j];
-            }
-        }
-
-    }
-    
-
-    return c[n];
+int print_soln(int p[], int n){
+    int k;
+    if(p[n]==1){ k = 1; }
+    else{ k = print_soln(p, p[n]-1) + 1; }
+    cout<<"line "<<k<<": from "<<p[n]<<" to "<<n<<"\n";
+    return k;
 }
 
+void solveWordWrap(int l[], int n, int m){
+    
+    int lc[n+1][n+1];
+    int spaces[n+1][n+1];
+    int dp[n+1];
+    int p[n+1];
+    
+    for(int i=0;i<=n;i++){
+        dp[i] = INF;
+        for(int j=0;j<=n;j++){
+          spaces[i][j] = INF;
+          lc[i][j] = INF;
+        }
+    }
+    
+    for(int i=1;i<=n;i++){
+        spaces[i][i] = m - l[i-1];
+        for(int j=i+1;j<=n;j++){
+            spaces[i][j] = spaces[i][j-1] - l[j-1] -1;
+        }
+    }
+    
+    for(int i=1;i<=n;i++){
+        for(int j=i;j<=n;j++){
+            if(spaces[i][j]>0){lc[i][j] = spaces[i][j]*spaces[i][j];}
+            else
+            if(j==n && spaces[i][j]>=0){lc[i][j] = 0;}                  // spaces after including the last word getting ignored
+            else
+            if(spaces[i][j]<0){lc[i][j] = INF;}
+        }
+    }
+    
+    dp[0] = 0;
+    for(int j=1;j<=n;j++){
+        dp[j] = INF;
+        for(int i=1;i<=j;i++){
+            if(dp[i-1]!=INF && lc[i][j]!=INF && dp[j]>dp[i-1]+lc[i][j]){
+                dp[j] = dp[i-1]+lc[i][j];
+                p[j] = i;
+            }
+        }
+    }
+    
+    int x = print_soln(p, n);
+    return;
+}
 
-
-int main(){
-
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
+int main()
+{
     int l[] = {3, 2, 2, 5};
     int n = sizeof(l)/sizeof(l[0]);
-    int M=6;
-
-    cout<<word_wrap(l, n, M);
-    
+    int M = 6;
+    solveWordWrap (l, n, M);
     return 0;
 }
