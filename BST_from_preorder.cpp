0f12d4b018caf875cd93be471638fada8e990423
aditya_jha4 <aditya.jha@rapyuta-robotics.com>
2021-08-02 02:53:49 +0530

    refactored BST_from_preorder

--- BST_from_preorder.cpp
+++ BST_from_preorder.cpp
@@ -1,60 +1,58 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long int
-#define M 1000005
-#define pp pair<int, int> 
-
-int tree[4*M];
-vector<pp> vec;
-
-
-void construct_tree(int pre[], int n){
-
-    stack<pp> st;
-    st.push({pre[0], 0});
-    tree[0] = pre[0];
-
-    for(int i=1;i<n;i++){
-
-        int temp = pre[i];
-        int root = -1;
-        int root_index = -1;
-
-        while(!st.empty() && temp>st.top().first){
-            root = st.top().first;
-            root_index = st.top().second;
-            st.pop();
-        }
-
-        if(root!=-1){
-            tree[2*root_index+2] = temp;
-            st.push({root, 2*root_index+2});
-        }
-        else{
-            root_index = st.top().second;
-            tree[2*root_index+1] = temp;
-            st.push({temp, 2*root_index+1});
-        }
-
+class Node{
+  
+  public:
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
     }
     
+};
+
+Node* constructTree(int pre[], int min_val, int max_val, int* i, int n){
+    
+    if(*i>n){ return NULL; }
+    
+    Node* root = NULL;
+    int curr = pre[*i];
+    
+    if(curr>min_val && curr<max_val){
+        root = new Node(curr);
+        if(*i<=n){
+            *i = *i + 1;
+            root->left = constructTree(pre, min_val, curr, i, n);
+            root->right = constructTree(pre, curr, max_val, i, n);
+        }
+    }
+    return root;
+}
+
+
+void printInorder(Node* root){
+    if(!root){return;}
+    if(root->left){printInorder(root->left);}
+    cout<<root->data<<" ";
+    if(root->right){printInorder(root->right);}
     return;
 }
 
-void show_tree(int n){
-    for(int i=0;i<4*n;i++){cout<<tree[i]<<" ";}
-    cout<<"\n";
-    return;
-}
+int main() {
+    
+    int pre[] = { 10, 5, 1, 7, 40, 50 };
+    int n = sizeof(pre) / sizeof(pre[0]);
 
-
-int main(){
-
-    int pre[] = {10, 5, 1, 7, 40, 50};
-    int n = sizeof(pre)/sizeof(pre[0]);
-
-    construct_tree(pre, n);
-    show_tree(n);
+    int index = 0;
+    Node* root = constructTree(pre, INT_MIN, INT_MAX, &index, n-1);
+ 
+    cout << "Inorder traversal: ";
+    printInorder(root);
+    
     return 0;
 }
